c341d048dd2e7d683539db81d2f426a464ac8651
Raphael Stadler <raphael.stadler@cmiag.ch>
2021-11-03 13:49:24 +0100

    entity framework core

---
+++ Entities/StackUnderflowContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StackUnderflow.Entities
+{
+    public class StackUnderflowContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+
+        public string DbPath { get; }
+
+        public StackUnderflowContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}stackunderflow.db";
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
